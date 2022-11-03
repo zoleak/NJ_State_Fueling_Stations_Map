e652834e763350713864de7748fedbb8db07b4be
zoleak <36116239+zoleak@users.noreply.github.com>
2022-11-03 11:26:05 -0400

    Updates to UI and server. Added geolocation

--- app.R
+++ app.R
@@ -14,6 +14,7 @@ library(dplyr)
 library(tidyr)
 library(shinyjs)
 library(leaflegend)
+library(geoloc)
 ###############################################################################
 ### Read in site information dataset for monitoring sites in NJ ###
 sites_nj<-read_csv("Ambient_Air_Quality_Monitors_of_New_Jersey.csv",col_names = T)%>%
@@ -22,8 +23,7 @@ sites_nj<-read_csv("Ambient_Air_Quality_Monitors_of_New_Jersey.csv",col_names = 
 ### Read in fueling stations info ###
 #fueling_nj2<-read_xlsx("fueling_stations_info.xlsx",col_names = T)%>%
 # dplyr::mutate(LON = as.numeric(LON))%>%
- # dplyr::mutate(LAT = as.numeric(LAT))
-
+# dplyr::mutate(LAT = as.numeric(LAT))
 ### Web scrape NJ fueling station website to access each stations data using rvest
 url<-"https://www.nj.gov/treasury/administration/statewide-support/motor-fuel-locations.shtml"
 css_selector<-"#budget"
@@ -38,61 +38,41 @@ fueling_nj$Location<-str_remove(fueling_nj$Location,"Longitude:")
 #Separate Location column into LON and LAT columns so we can plot locations on leaflet
 fueling_nj<-fueling_nj%>%
   mutate(LON = as.numeric(str_extract(string = Location,
-                           pattern = "-.*")),
+                                      pattern = "-.*")),
          LAT = as.numeric(str_extract(string = Location,
-                           pattern = "39.*|40.*|41.*")))
+                                      pattern = "39.*|40.*|41.*")))
 ### Read in shapefile of NJ counties ###
 counties_nj<-st_read(dsn = getwd(),layer = "NJ_counties")
 ### Change projection to work with leaflet map ###
 counties_nj<-st_transform(counties_nj, crs="+init=epsg:4326")
 ###############################################################################
-# Function for  allowing geolocalisation in leaflet map
-jsCode <- '
-shinyjs.geoloc = function() {
-navigator.geolocation.getCurrentPosition(onSuccess, onError);
-function onError (err) {
-Shiny.onInputChange("geolocation", false);
-}
-function onSuccess (position) {
-setTimeout(function () {
-var coords = position.coords;
-console.log(coords.latitude + ", " + coords.longitude);
-Shiny.onInputChange("geolocation", true);
-Shiny.onInputChange("lat", coords.latitude);
-Shiny.onInputChange("long", coords.longitude);
-}, 5)
-}
-};
-'
 ### Define UI for application ###
 ui <- fluidPage(
-   ### Application title ###
+  ### Application title ###
   h1(id="big-heading", "NJ State Fueling Stations/Air Quality Stations"),
   tags$style(HTML("#big-heading{color: #074dc3;text-align: center;background-color: #D3D3D3;}
                   html {
-             position: relative;
-             min-height: 100%;
-           }
-           body {
-             margin-bottom: 60px; /* Margin bottom by footer height */
-           }
-           .footer {
-             position: absolute;
-             bottom: 0;
-             width: 100%;
-             height: 60px; /* Set the fixed height of the footer here */
-             background-color: #f5f5f5;
-           }")),
-# Tell shiny we will use some Javascript
-  useShinyjs(),
-  extendShinyjs(text = jsCode, functions = c("geoloc")),
-  # One button and one map
-  #br(),
-  #actionButton("geoloc", "Localize me", class="btn btn-primary", onClick="shinyjs.geoloc()"),
-  
-   ### Leaflet output ###
-   leafletOutput("fuelmap", height = "95vh")%>%
-     withSpinner(type = 5, color = "blue"),
+                  position: relative;
+                  min-height: 100%;
+                  }
+                  body {
+                  margin-bottom: 60px; /* Margin bottom by footer height */
+                  }
+                  .footer {
+                  position: absolute;
+                  bottom: 0;
+                  width: 100%;
+                  height: 60px; /* Set the fixed height of the footer here */
+                  background-color: #f5f5f5;
+                  }")),
+### Leaflet output ###
+h2(HTML("Where Am I?"), 
+   style="text-align:center"),
+tags$p("Click the button to get your location",style="text-align:center"),
+div(geoloc::button_geoloc("myBtn", "Get my Location"),style ="text-align:center"),
+tags$br(),
+leafletOutput("fuelmap", height = "95vh")%>%
+  withSpinner(type = 5, color = "blue"),
 tags$footer(
   "NJ State Vehicle Fueling Station Website:",
   class = "footer",
@@ -103,7 +83,7 @@ tags$footer(
   )))
 ###############################################################################
 ### Define server logic ###
-server <- function(input, output) {
+server <- function(input, output,session) {
   # Create custom icon for leaflet map
   # Icons for air monitoring stations
   air_Icon <- makeIcon(
@@ -124,34 +104,29 @@ server <- function(input, output) {
     shadowAnchorX = 4, shadowAnchorY = 62
   )
   
-### Create map of ambient air monitoring stations in NJ ###
+  ### Create map of ambient air monitoring stations in NJ ###
   output$fuelmap<-renderLeaflet({
-    leaflet(data = sites_nj, options = leafletOptions(minZoom = 7))%>%
+    leaflet(data = sites_nj, options = leafletOptions(minZoom = 8))%>%
       addTiles()%>%
       addResetMapButton()%>%
-      #addControlGPS(options = gpsOptions(position = "topleft", activate = TRUE, 
-      #                                   autoCenter = TRUE, maxZoom = 60, 
-       #                                  setView = TRUE))%>%
-      addTiles(group = "OSM (default)") %>%
+      addTiles(group = "OSM (default)")%>%
       addProviderTiles(providers$Esri.WorldImagery, group = "Satellite")%>%
       addPolygons(data = counties_nj,popup= paste("County:",counties_nj$COUNTY,sep=""),color = "black",
                   weight = 1,
                   highlightOptions = highlightOptions(color = "blue",
                                                       weight = 2,bringToFront = TRUE))%>%
       setView(lng = -74.4 ,lat =40, zoom = 8)%>%
-      addControl(actionButton("geoloc", "Find My Location", class="btn btn-primary", 
-                               onClick="shinyjs.geoloc()"))%>%
       addMarkers(~LON,~LAT, popup = ~paste("<h4> Site Name:</h4>",SITE_NAME,
                                            "<h4> Address:</h4>",ADDRESS,sep = ""),
                  label = ~SITE_NAME,
                  icon = air_Icon,
                  clusterOptions = markerClusterOptions())%>%
       addMarkers(data = fueling_nj,~LON,~LAT,
-                        popup = ~paste("<h6> County:</h4>",County,
-                                       "<h6> Hours:</h4>",Hours,
-                                       "<h6> Phone Number:</h4>",`Phone Number`,
-                                       "<h6> Fuel Types:</h4>",`Fuel Types`,
-                                       "<h6> Address:</h4>",`Fueling Station Address`,sep = ""),
+                 popup = ~paste("<h6> County:</h4>",County,
+                                "<h6> Hours:</h4>",Hours,
+                                "<h6> Phone Number:</h4>",`Phone Number`,
+                                "<h6> Fuel Types:</h4>",`Fuel Types`,
+                                "<h6> Address:</h4>",`Fueling Station Address`,sep = ""),
                  icon = gas_Icon,
                  clusterOptions = markerClusterOptions())%>%
       addLegendImage(images = c("https://png.pngtree.com/png-vector/20190319/ourlarge/pngtree-vector-gas-icon-png-image_845941.jpg",
@@ -165,22 +140,18 @@ server <- function(input, output) {
         options = layersControlOptions(collapsed = FALSE))
   })
   
-  # Find geolocalisation coordinates when user clicks
-  observeEvent(input$geoloc, {
-    js$geoloc()
-  })
-  # zoom on the corresponding area
   observe({
-    if(!is.null(input$lat)){
-      map <- leafletProxy("fuelmap")
-      dist <- 0.2
-      lat <- input$lat
-      lng <- input$long
-      map %>% fitBounds(lng - dist, lat - dist, lng + dist, lat + dist)
-    }
+    req(input$myBtn_lon)
+    req(input$myBtn_lat)
+    click<-input$myBtn
+    if(is.null(click))
+      return()
+    else
+      leafletProxy("fuelmap", session)%>%
+      setView(as.numeric(input$myBtn_lon), as.numeric(input$myBtn_lat), zoom = 17)
+    
+    
   })
-
-  
   
 }
 ###############################################################################
